5427e62a1be8a9e08f82d17654d595ef144a6853
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-01-30 10:29:34 +0700

    fix layout v2

--- src/redux_store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// utk parent semua reducer
-import { combineReducers } from "redux";
-import listPost from "./postReducers";
-
-
-//mau export reducer ini
-const rootReducer = combineReducers({listPost,});
-
-export default rootReducer
